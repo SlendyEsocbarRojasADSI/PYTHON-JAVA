a28bf6ca6f5704c01622312db817edee5d00f0a1
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-16 11:46:18 -0500

    JAVA_03

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-let lado1 = parseInt(prompt("Ingresar el primer lado:"));
-let lado2 = parseInt(prompt("Ingresar el segundo lado:"));
+let lado1 = Number(prompt("Digite el primer laado del rectangulo::"));
+let lado2 = Number(prompt("Digite el segundo lado del rectangulo:"));
 
-alert('Programa para calcular media');
-let medi = 0
-for(let i=0; i<4; i++){
-    let num = parseInt(prompt("Ingresar un numero:"));
-    medi = num + medi
-}
-
+let num1 = parseInt(prompt("Ingrese el primer numero:"));
+let num2 = parseInt(prompt("Ingrese el segundo numero:"));
+let num3 = parseInt(prompt("Ingrese el tercer numero:"));
+let num4 = parseInt(prompt("Ingrese el cuarto numero:"));
 
 let area = lado1*lado2;
+let media = (num1+num2+num3+num4)/4;
 
-document.getElementById('medi').innerHTML=medi/4
 document.getElementById('area').innerHTML=area
+document.getElementById('media').innerHTML=media
