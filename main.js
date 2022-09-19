560569baead5c251cdaa45049364d74dc7c2f225
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 07:52:26 -0500

    JAVA_07

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-alert('Programa para calcular la longitud y el area de una circuferencia');
-
 let espa = parseInt(prompt("Ingresar el espacio del proyectil:"));
-let tiempo = parseInt(prompt("Ingresar el tie,po del proyectil:"));
+let tiempo = parseInt(prompt("Ingresar el tiempo del proyectil:"));
+let radio = parseInt(prompt("Ingresar el radio de la esfera:"));
 
-let radio = parseInt(prompt("Ingresar el radio de la circuferencia:"));
 let velo = (espa*1000)/(tiempo*60);
-
-let longi = 2*Math.PI*radio;
-let area = Math.PI*Math.pow(radio,2);
-
-document.getElementById('longi').innerHTML=longi;
-document.getElementById('area').innerHTML=area;
+let volu = 4/3*Math.PI*radio**3;
 
 document.getElementById('velo').innerHTML=velo;
+
+document.getElementById('volu').innerHTML=volu;
