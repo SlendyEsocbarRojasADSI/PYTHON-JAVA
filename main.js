7e0eb8a8e8ef424df8ddd679011ccc1c7748596f
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 08:13:28 -0500

    JAVA_09

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let radio = parseInt(prompt("Ingresar el radio de la esfera:"));
+let radio = parseInt(prompt("Ingresar el radio del cilindro:"));
+let altu = parseInt(prompt("Ingresar la altura del cilindro:"));
 
-let a = parseInt(prompt("Ingresar el valor de A:"));
-let b = parseInt(prompt("Ingresar el valor de B:"));
-let c = parseInt(prompt("Ingresar el valor de C:"));
-
-let volu = 4/3*Math.PI*radio**3;
-let total = (a+7*c)/(b+2-a)+2*b;
+let volu = (Math.PI*radio**2)*altu;
+let area = (2*(Math.PI*radio**2))+((2*Math.PI*radio)*altu);
 
 document.getElementById('volu').innerHTML=volu;
-document.getElementById('expre').innerHTML=total
+document.getElementById('area').innerHTML=area;
