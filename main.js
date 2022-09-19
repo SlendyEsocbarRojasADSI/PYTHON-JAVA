5b9aa0d7a5bf34929ea6ab71177c21bf320b50c0
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 07:45:41 -0500

    JAVA_06

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-let base = parseInt(prompt("Ingresar la base del rectangulo:"));
-let altu = parseInt(prompt("Ingresar la altura del triangulo:"));
-
 alert('Programa para calcular la longitud y el area de una circuferencia');
 
-let area = (base*altu)/2
+let espa = parseInt(prompt("Ingresar el espacio del proyectil:"));
+let tiempo = parseInt(prompt("Ingresar el tie,po del proyectil:"));
+
 let radio = parseInt(prompt("Ingresar el radio de la circuferencia:"));
+let velo = (espa*1000)/(tiempo*60);
 
 let longi = 2*Math.PI*radio;
-let are = Math.PI*Math.pow(radio,2);
+let area = Math.PI*Math.pow(radio,2);
 
 document.getElementById('longi').innerHTML=longi;
 document.getElementById('area').innerHTML=area;
-document.getElementById('are').innerHTML=are
+
+document.getElementById('velo').innerHTML=velo;
