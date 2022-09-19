7806d2fa6dc0fd86cdad6d22c70236bdb91a3641
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 07:30:17 -0500

    JAVA_05

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-alert('Programa para calcular media');
-
-let medi=0
-for (let i=0;i<4;i++){
-    let num = parseInt(prompt("Ingresar la base del triangulo:"));
-    medi = num+ medi;
-}
-
-let base = parseInt(prompt("Ingrese la base del triangulo:"));
+let base = parseInt(prompt("Ingresar la base del rectangulo:"));
 let altu = parseInt(prompt("Ingresar la altura del triangulo:"));
 
-document.getElementById('medi').innerHTML=medi/4
+alert('Programa para calcular la longitud y el area de una circuferencia');
 
-let area = (base*altu)/2;
+let area = (base*altu)/2
+let radio = parseInt(prompt("Ingresar el radio de la circuferencia:"));
 
+let longi = 2*Math.PI*radio;
+let are = Math.PI*Math.pow(radio,2);
+
+document.getElementById('longi').innerHTML=longi;
 document.getElementById('area').innerHTML=area;
+document.getElementById('are').innerHTML=are
