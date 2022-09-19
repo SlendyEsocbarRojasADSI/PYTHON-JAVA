390844a939334f03cbb036151e9ce0b4d5af0286
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 11:14:55 -0500

    JAVA_20

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-let nom = parseInt(prompt("Ingresar el nombre del empleado:"));
-let horas = parseInt(prompt("Ingresar las horas trabajadas por el empleado:"));
 let notaF = 0;
 
 for (let i=1; i<=4; i++){
@@ -8,10 +6,21 @@ for (let i=1; i<=4; i++){
 }
 
 let prome = notaF/4;
-let pagar = horas*15300;
-
-document.getElementById('nom').innerHTML=nom;
-document.getElementById('horas').innerHTML=horas;
-document.getElementById('pagar').innerHTML=pagar;
+let sueldo = parseInt(prompt("Ingresar el sueldo del empleado:"));
+let venta1 = parseInt(prompt("Ingresar el precio de la venta 1:"));
+let venta2 = parseInt(prompt("Ingresar el precio de la venta 2:"));
+let venta3 = parseInt(prompt("Ingresar el precio de la venta 3:"));
 
 document.getElementById('prome').innerHTML=prome;
+
+let comi1 = venta1*0.10;
+let comi2 = venta2*0.10;
+let comi3 = venta3*0.10;
+
+let sueldoTotal = sueldo+comi1+comi2+comi3;
+
+document.getElementById('comi1').innerHTML=comi1;
+document.getElementById('comi2').innerHTML=comi2;
+document.getElementById('comi3').innerHTML=comi3;
+
+document.getElementById('sueldo').innerHTML=sueldoTotal;
