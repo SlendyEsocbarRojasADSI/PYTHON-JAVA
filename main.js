60ca453cc3446baff6c28319902022c5d9b91f9b
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 11:06:21 -0500

    JAVA_19

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-let iva = parseInt(prompt("Ingresar el iva del producto sin %:"));
-let produ = parseInt(prompt("Ingresar el precio del producto:"));
 let nom = parseInt(prompt("Ingresar el nombre del empleado:"));
 let horas = parseInt(prompt("Ingresar las horas trabajadas por el empleado:"));
+let notaF = 0;
 
-let ivaP = produ*(iva/100);
-let total = produ+ivaP;
+for (let i=1; i<=4; i++){
+    let nota = parseInt(prompt("Digitar las notas:"));
+    nota = nota+notaF;
+}
+
+let prome = notaF/4;
 let pagar = horas*15300;
-
-document.getElementById('pagar').innerHTML=total;
-document.getElementById('iva').innerHTML=ivaP;
 
 document.getElementById('nom').innerHTML=nom;
 document.getElementById('horas').innerHTML=horas;
 document.getElementById('pagar').innerHTML=pagar;
+
+document.getElementById('prome').innerHTML=prome;
