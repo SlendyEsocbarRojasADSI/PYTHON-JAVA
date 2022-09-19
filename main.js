fd361153b826ce5fc439b219672ba7a9de5ad3e2
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 10:11:06 -0500

    JAVA_15

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-let cantiMed = parseInt(prompt("Ingresar la cantidad de medicamentos:"));
-let totalMed=0;
-
-for (let i=0; i<cantiMed; i++){
-    let precioMed = parseInt(prompt("Ingresar el precio del producto ${i+1}"));
-    totalMed = precioMed+totalMed;
-}
-
-let minu = parseInt(prompt("Ingresar la cantidad de minutos hablados:"));
-let pagar = minu*precioMed+((minu*precioMed)*0.2);
-
-document.getElementById('pagar').innerHTML=pagar;
+let grados = parseInt(prompt("Ingresar los grados en celcius:"));
 
 let descu = totalMed*0.10;
 let precioFin = totalMed-descu;
+
 console.log(descu)
 
-document.getElementById('precio').innerHTML=precioFin;
 document.getElementById('descu').innerHTML=descu;
+document.getElementById('precio').innerHTML=precioFin;
+
+let conversion = (grados*9/5)+32;
+
+document.getElementById('conversion').innerHTML=conversion;
