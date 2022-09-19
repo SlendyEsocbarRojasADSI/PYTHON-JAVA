f93f88cb3a7481c33cc9bce98311d8c86e5b2430
SlendyEsocbarRojasADSI <sleppymorningxd@gmail.com>
2022-09-19 10:33:11 -0500

    JAVA_16

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 let grados = parseInt(prompt("Ingresar los grados en celcius:"));
-
-let descu = totalMed*0.10;
-let precioFin = totalMed-descu;
-
-console.log(descu)
-
-document.getElementById('descu').innerHTML=descu;
-document.getElementById('precio').innerHTML=precioFin;
+let parcial1 = parseInt(prompt("Ingresar la nota del parcial 1:"));
+let parcial2 = parseInt(prompt("Ingresar la nota del parcial 2:"));
+let parcial3 = parseInt(prompt("Ingresar la nota del parcial 3:"));
+let examenF = parseInt(prompt("Ingresar la nota del examen final:"));
+let trabajoF = parseInt(prompt("Ingresar la nota del trabajo final:"));
 
 let conversion = (grados*9/5)+32;
+let nota = (((parcial1+parcial2+parcial3)/3)*0.55)+(examenF*0.3)+(trabajoF*0.15);
 
 document.getElementById('conversion').innerHTML=conversion;
+document.getElementById('nota').innerHTML=nota;
